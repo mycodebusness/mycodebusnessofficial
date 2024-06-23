cc59de7360174d0b043fb99d4b33351b90a6b0b6
Ir Isaac ILLUNGA <isaacilungaofficial@gmail.com>
2024-06-23 08:59:18 +0200

    quatre commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,7 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
-import { Fira_Code } from "next/font/google";
 
-export const firaCode = Fira_Code({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  style: ["normal"],
-});
 const roboto = Roboto({
   weight: ["300", "400", "500", "700", "900"],
   subsets: ["latin"],
